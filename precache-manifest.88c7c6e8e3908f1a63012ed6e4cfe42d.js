9447b5b4d30f0b9e2ecf88523f04067935e50317
poengen <pal.oskar.engen@gmail.com>
2020-01-13 09:22:16 +0100

    Updates

---
+++ precache-manifest.88c7c6e8e3908f1a63012ed6e4cfe42d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d63d0cdd0fe72884e95286d07f6bbba",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "fbf2df9e7671f3027053",
+    "url": "/videos/static/css/main.81d7c035.chunk.css"
+  },
+  {
+    "revision": "37d54a0ccd966f5d384b",
+    "url": "/videos/static/js/2.acc1704d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/videos/static/js/2.acc1704d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbf2df9e7671f3027053",
+    "url": "/videos/static/js/main.92e4b4a7.chunk.js"
+  },
+  {
+    "revision": "c9b251da99ddc3a9390c",
+    "url": "/videos/static/js/runtime-main.7f5920a8.js"
+  }
+]);
